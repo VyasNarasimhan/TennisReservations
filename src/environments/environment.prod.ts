b86a906147268dd6c82c41f55ef37b8ff9870306
Vyas Narasimhan <reach.vyas.2003@gmail.com>
2021-02-15 18:17:47 -0500

    http

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiRoot: 'https://ec2-18-208-214-113.compute-1.amazonaws.com:4201'
+  apiRoot: 'http://ec2-18-208-214-113.compute-1.amazonaws.com:4201'
 };
